76e371f14d4e9295c5e5c4fe22106c295ad9c16b
Kimberly Lord <kalord93@gmail.com>
2020-06-20 12:53:20 -0600

    Add imports to models and views for authentication and CBV

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def home(request):
-    return HttpResponse('home')
+    return render(request, 'home.html')
+
