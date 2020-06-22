8a3a1171c9877982115a058a85a5492f4fa52df2
TCashion <58757351+TCashion@users.noreply.github.com>
2020-06-22 15:02:06 -0600

    add Accesory model

--- main_app/models.py
+++ main_app/models.py
@@ -52,30 +52,32 @@ class Instrument(models.Model):
         return reverse('instruments_detail', kwargs={'instrument_id' : self.id})
 
 
-# class Accessory(models.Model):
-#     name = models.CharField(max_length=150)
-#     manufacturer = models.CharField(max_length=150)
-#     year = models.IntegerField('Manufactur year (YYYY)',
-#         null=True,
-#         blank=True,
-#         min_value=1900,
-#         max_value=max_year(), 
-#     )
-#     serial = models.CharField(max_length=250, blank=True, null=True)
-#     price = models.DecimalField(
-#         max_digits=12, 
-#         decimal_places=2,
-#         validators = [MinValueValidator(0.01, 'Price must be greater than $0.01')],
-#     )
-#     condition = models.CharField(
-#         max_length = 1,
-#         choices = CONDITIONS,
-#         default = CONDITIONS[0][0]
-#     )
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+class Accessory(models.Model):
+    name = models.CharField(max_length=150)
+    accessory_type = models.CharField(max_length=150)
+    manufacturer = models.CharField(max_length=150)
+    year = models.IntegerField('Manufacture year (YYYY)',
+        null=True,
+        blank=True,
+        validators=[
+            MinValueValidator(1900, 'Year must be greater than 1900'),
+            MaxValueValidator(max_year(), f"Year must be less than {max_year()}")]
+    )
+    serial = models.CharField(max_length=250, blank=True, null=True)
+    price = models.DecimalField(
+        max_digits=12, 
+        decimal_places=2,
+        validators = [MinValueValidator(0.01, 'Price must be greater than $0.01')],
+    )
+    condition = models.CharField(
+        max_length = 1,
+        choices = CONDITIONS,
+        default = CONDITIONS[0][0]
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return f"Name: {self.name}, Type: {self.accessory_type}"
+    def __str__(self):
+        return f"Name: {self.name}"
     
-#     def get_absolute_url(self):
-#         return reverse('accessories_detail', kwargs={'accessory_id' : self.id})
+    def get_absolute_url(self):
+        return reverse('accessories_detail', kwargs={'accessory_id' : self.id})
